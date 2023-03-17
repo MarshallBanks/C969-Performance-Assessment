7a2da10c4e8756a8382b16906cafac80384e70d2
MegaMarsh <marshallmelonhead@hotmail.com>
2023-03-17 11:27:18 -0700

    Started the GUI for the scheduling form.

---
+++ SchedulingForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace C969_Performance_Assessment
+{
+    partial class SchedulingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appointmentDGV = new System.Windows.Forms.DataGridView();
+            this.monthlyButton = new System.Windows.Forms.RadioButton();
+            this.weeklyButton = new System.Windows.Forms.RadioButton();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // appointmentDGV
+            // 
+            this.appointmentDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.appointmentDGV.Location = new System.Drawing.Point(130, 64);
+            this.appointmentDGV.Name = "appointmentDGV";
+            this.appointmentDGV.Size = new System.Drawing.Size(618, 344);
+            this.appointmentDGV.TabIndex = 0;
+            // 
+            // monthlyButton
+            // 
+            this.monthlyButton.AutoSize = true;
+            this.monthlyButton.Location = new System.Drawing.Point(133, 41);
+            this.monthlyButton.Name = "monthlyButton";
+            this.monthlyButton.Size = new System.Drawing.Size(88, 17);
+            this.monthlyButton.TabIndex = 1;
+            this.monthlyButton.TabStop = true;
+            this.monthlyButton.Text = "Monthly View";
+            this.monthlyButton.UseVisualStyleBackColor = true;
+            // 
+            // weeklyButton
+            // 
+            this.weeklyButton.AutoSize = true;
+            this.weeklyButton.Location = new System.Drawing.Point(227, 41);
+            this.weeklyButton.Name = "weeklyButton";
+            this.weeklyButton.Size = new System.Drawing.Size(87, 17);
+            this.weeklyButton.TabIndex = 2;
+            this.weeklyButton.TabStop = true;
+            this.weeklyButton.Text = "Weekly View";
+            this.weeklyButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(25, 64);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(25, 93);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(25, 122);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // SchedulingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.weeklyButton);
+            this.Controls.Add(this.monthlyButton);
+            this.Controls.Add(this.appointmentDGV);
+            this.Name = "SchedulingForm";
+            this.Text = "Scheduling Appointment Book";
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView appointmentDGV;
+        private System.Windows.Forms.RadioButton monthlyButton;
+        private System.Windows.Forms.RadioButton weeklyButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
